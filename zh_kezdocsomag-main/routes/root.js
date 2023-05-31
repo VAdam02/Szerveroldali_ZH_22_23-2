a3a74c5a6c53a6935cdc087603aac2fbfed9fa06
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-05-31 15:43:27 +0200

    restapi min 8.5 point

--- zh_kezdocsomag-main/routes/root.js
+++ zh_kezdocsomag-main/routes/root.js
@@ -3,7 +3,7 @@ const S = require("fluent-json-schema");
 const db = require("../models");
 const { Sequelize, sequelize } = db;
 const { ValidationError, DatabaseError, Op } = Sequelize;
-// TODO: Importáld a modelleket
+const { Location, Weather, Warning } = require("../models");
 // const { /* modellek importálása itt */ } = db;
 
 module.exports = function (fastify, opts, next) {
@@ -22,6 +22,108 @@ module.exports = function (fastify, opts, next) {
     fastify.get("/auth-protected", { onRequest: [fastify.auth] }, async (request, reply) => {
         reply.send({ user: request.user });
     });
+
+
+    fastify.get(
+        "/locations",
+        async (request, reply) => {
+            const locations = await Location.findAll();
+            return reply.status(200).send(locations);
+        }
+    );
+
+    fastify.get(
+        "/locations/:id",
+        {
+            schema: {
+                params: {
+                    type: "object",
+                    properties: {
+                        id: { type: "integer" },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            const location = await Location.findByPk(request.params.id);
+            if (!location) return reply.status(404).send({ message: "Location not found." });
+            return reply.status(200).send(location);
+        }
+    );
+
+    fastify.post(
+        "/locations",
+        {
+            schema: {
+                body: {
+                    type: "object",
+                    required: ["name", "lat", "lon"],
+                    properties: {
+                        name: { type: "string" },
+                        lat: { type: "number" },
+                        lon: { type: "number" },
+                        public: { type: "boolean", nullable: true, default: true }
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            if (await Location.findByPk(request.params.id) != null) { return reply.status(500); }
+            const location = await Location.create(request.body);
+            return reply.status(201).send(location);
+        }
+    );
+
+    fastify.delete(
+        "/locations/:id",
+        {
+            schema: {
+                params: {
+                    type: "object",
+                    properties: {
+                        id: { type: "integer" },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            const location = await Location.findByPk(request.params.id);
+            if (!location) return reply.status(404).send({ message: "Location not found." });
+            await location.destroy();
+            return reply.status(200).send();
+        }
+    );
+
+    fastify.post(
+        "/login",
+        {
+            schema: {
+                body: {
+                    type: "object",
+                    required: ["email"],
+                    properties: {
+                        email: { type: "string" },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            const id = request.body.email.substr(8,request.body.email.split('@')[0].length-8);
+            if (!(request.body.email.split('@')[1] === "weather.org"
+            && !isNaN(id)
+            && (request.body.email).substr(0, 8) === "location"))
+            {
+                return reply.status(418).send();
+            }
+
+            const location = await Location.findByPk(id);
+            if (!location) return reply.status(404).send({ message: "Location not found." });
+
+            const token = fastify.jwt.sign(location.toJSON())
+
+            return reply.status(200).send({ token });
+        }
+    );
 
     next();
 };
